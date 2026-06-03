192a0b6c32fe22ba9eb3388d0bf6c97ba8dcf5dc
Saheem <saheem.nakhwa24@gmail.com>
2026-06-03 22:35:37 +0530

    :)

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -3,7 +3,10 @@ import { Avatar, AvatarImage, AvatarFallback } from './ui/avatar'
 import { Dialog, DialogContent, DialogTrigger } from './ui/dialog'
 import { MoreHorizontal } from 'lucide-react'
 import { Button } from './ui/button'
-
+import { FaHeart, FaRegHeart } from "react-icons/fa";
+import { FiMessageCircle } from "react-icons/fi";
+import { IoIosSend } from "react-icons/io";
+import { FaRegBookmark } from "react-icons/fa6";
 
 const Post = () => {
     return (
@@ -34,14 +37,24 @@ const Post = () => {
             </div>
             <img
                 className='rounded-sm my-2 w-full aspect-square object-cover'
-                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcAOZ0UcyYsrRhxUkZk6nG66WTLg-NhiTEcg&s" alt="" 
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcAOZ0UcyYsrRhxUkZk6nG66WTLg-NhiTEcg&s" alt=""
             />
 
-            <div>
-                
+
+
+            <div className='flex items-center justify-between my-2'>
+                <div className='flex items-center gap-3'>
+                    <FaRegHeart className='cursor-pointer hover:text-gray-600' />
+                    <FiMessageCircle className='cursor-pointer hover:text-gray-600' />
+                    <IoIosSend className='cursor-pointer hover:text-gray-600' />
+                </div>
+
+                <FaRegBookmark className='cursor-pointer hover:text-gray-600' />
             </div>
 
         </div>
+
+
     )
 }
 
